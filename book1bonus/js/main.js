d244f751dc2f3fac111700c6535c7f6fcfa0d271
cosmolady <katerina-2310@ya.ru>
2017-06-02 13:27:13 +0300

    add freshmail.com to email form

--- book1bonus/js/main.js
+++ book1bonus/js/main.js
@@ -14,16 +14,10 @@ $(document).ready(function () {
         'visibility': 'hidden'
     });
     arrowUp.removeClass('none');
-    // Add smooth scrolling to all links
     $("a").on('click', function (event) {
-        // Make sure this.hash has a value before overriding default behavior
         if (this.hash !== "") {
-            // Prevent default anchor click behavior
             event.preventDefault();
-            // Store hash
             var hash = this.hash;
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
             }, 800, function () {});
@@ -45,4 +39,3 @@ $(document).ready(function () {
         }
     });
 });
-//document.body.addEventListener('touchmove', function(e){ e.preventDefault(); });
